e088634434da8a9a5b35809eb5b254c96f95cb75
AdrienBot <r4dst4rspam@gmail.com>
2023-07-19 17:15:49 +0200

    Change to firefox driver

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,14 +1,14 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { Builder, By, until, Capabilities } = require('selenium-webdriver');
 const { expect } = require('chai');
-const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 
 let driver;
 
 Given('I am on the main page', async function() {
-  let chromeOptions = new chrome.Options();
-  chromeOptions.addArguments("--headless");
-  driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
+  let firefoxOptions = new firefox.Options();
+  //firefoxOptions.addArguments("--headless");
+  driver = await new Builder().forBrowser('firefox').setFirefoxOptions(firefoxOptions).build();
   await driver.get('http://localhost:3000');
 });
 
@@ -18,6 +18,8 @@ When('I click on the "privacy" button', async function() {
 });
 
 Then('I should be redirected to the privacy tab', async function() {
+  // Wait until the URL contains '/privacy' before proceeding
+  await driver.wait(() => driver.getCurrentUrl().then(url => url.includes('/privacy')), 10000);
   let currentUrl = await driver.getCurrentUrl();
   expect(currentUrl).to.include('/privacy');
 
